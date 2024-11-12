eaa8e5ebd11ebd4822545c9e6b7f776bfd44489f
annekarol2908 <anneakarol.oliveirabatista@gmail.com>
2024-11-12 08:26:44 -0300

    primeiro commit

---
+++ System_Treasure/src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app theme="light">
+    <v-conatiner>
+      <v-row align="center" justify="center">
+        <div class="square">
+        </div>
+      </v-row>
+    </v-conatiner>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'MyComponent'
+}
+</script>
---
+++ System_Treasure/src/pages/login/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-sheet class="mx-auto" width="350">
+      <v-form fast-fail @submit.prevent>
+        <v-container class="flex-column">
+            
+        <v-text-field
+          v-model="ID"
+          :rules="idRules"
+          label="ID de entrada"
+          
+        ></v-text-field>
+  
+        <v-text-field
+          v-model="senha"
+          :rules="senhaRules"
+          label="Senha"
+        ></v-text-field>
+  
+        <v-btn class="mt-2" type="submit" block>Entrar</v-btn>
+        </v-container>
+      </v-form>
+    </v-sheet>
+  </template>
+  <script>
+    export default {
+      data: () => ({
+        ID: '',
+        idRules: [
+          value => {
+            if (value?.length >= 3) return true
+  
+            return 'Digite seu id para entrar.'
+          },
+        ],
+        senha: '',
+        senhaRules: [
+          value => {
+            if (/[^0-9]/.test(value)) return true
+  
+            return 'Digite sua senha para entrar.'
+          },
+        ],
+      }),
+    }
+  </script>
+  
